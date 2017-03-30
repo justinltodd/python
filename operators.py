352d6e45402b84d5573a6d5774e4692e829a9a8c
justinltodd <Justin@JUSTINs-MacBook-Pro.local>
2017-03-30 18:40:41 -0500

    Add,Subtract,Multiplication,Division

---
+++ operators.py
@@ -0,0 +1,10 @@
+#!/Users/Justin/anaconda/bin/python
+# OPERATORS  - AUTH JUSTIN TODD
+x = 8
+y = 7
+
+
+print "Addition:" , x + y
+print "Subtraction:" , x - y
+print "Multiplication:" , x * y
+print "Division:" , x % y
